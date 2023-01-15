e6b5f70bdefeeb2978078446546196b5d5376c52
LucDuong1720 <lucduong1720@gmail.com>
2023-01-15 21:29:43 +0700

    studentmanagement

--- student.js
+++ student.js
@@ -9,11 +9,22 @@ class Information {
     }
 }
 
-let information = [
-    new Information (18234, "Nguyễn Văn A", "nguyenvana@gmail.com", "0786538297", "129 Nguyễn Trường Tộ", "Nam" ),
-    new Information (18235, "Trần Văn B", "tranvanb@gmail.com", "0908634227", "35 Điện Biên Phủ", "Nam" ),
-    new Information (18236, "Nguyễn Thị C", "nguyenthic@gmail.com", "0648375942", "32 Bùi Thị Xuân", "Nữ" )
-]
+let information = [];
+
+const data = "data";
+
+function saveData() {
+    if (localStorage.getItem(data) == null) {
+        information = [
+            new Information (18234, "Nguyễn Văn A", "nguyenvana@gmail.com", "0786538297", "129 Nguyễn Trường Tộ", "Nam" ),
+            new Information (18235, "Trần Văn B", "tranvanb@gmail.com", "0908634227", "35 Điện Biên Phủ", "Nam" ),
+            new Information (18236, "Nguyễn Thị C", "nguyenthic@gmail.com", "0648375942", "32 Bùi Thị Xuân", "Nữ" )
+        ]
+        localStorage.setItem(data, JSON.stringify(information)); 
+    } else {
+        information = JSON.parse(localStorage.getItem(data));
+    }
+}
 
 function renderInformation() {
     console.log=('a');
@@ -46,11 +57,15 @@ function createInformation() {
     let address = document.getElementById("address").value;
     let gender = document.getElementById("gender").value;
 
-    let informations = new Information (id, name, email, phone, address, gender );
-    information.push(informations);
-
+    if (id == "" || name == "" || email == "" || phone == "" || address == "" || gender == "") {
+        alert("Bạn cần nhập đầy đủ thông tin");
+    } else {
+    let new_information = new Information (id, name, email, phone, address, gender );
+    information.push(new_information);
+    localStorage.setItem(data, JSON.stringify(information));
     renderInformation();
     clearInformation();
+    }
 }
 
 function clearInformation() {
@@ -62,5 +77,6 @@ function clearInformation() {
     document.getElementById("gender").value = "";
 }
 
+saveData();
 renderInformation();
 
